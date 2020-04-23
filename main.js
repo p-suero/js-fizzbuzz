ccf647e8fa7d339414d6259cea67aee9633693c3
Paolo <paolo.suero@gmail.com>
2020-04-23 17:09:17 +0200

    Inserita traccia esercizio (esercizio completo

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, con le seguenti regole:
+// al posto dei multipli di 3 stampi "Fizz"
+// al posto dei multipli di 5 stampi "Buzz"
+// al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
+
 //creo una sequenza di numeri da 1 a 100
 for ( var i = 1; i <= 100; i++) {
     //devo individuare i valori che sono multipli di 3
